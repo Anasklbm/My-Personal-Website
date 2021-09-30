6d03e12159de24c2b859266e2115e2e684bb728f
Anas Y <87906254+Anasklbm@users.noreply.github.com>
2021-09-30 15:52:36 +0530

    Whatsapp added
    Added

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="assets/images/Anas_Trivandrum.pdf">Resume</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://wa.me/8111806818" target ="_blank">Whatsapp Me</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://wa.me/918111806818" target ="_blank">Whatsapp Me</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="mailto:anasriyasniyas@gmail.com">Hire Me</a></li>
    
    
